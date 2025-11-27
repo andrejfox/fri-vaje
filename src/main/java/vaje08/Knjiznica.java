18743cc545cdad5e478750d02921c04f56052468
andrejfox <andrej.vencelj.007@gmail.com>
2025-11-27 10:37:49 +0100

    vaje08 done! Hvala Nejc

--- src/main/java/vaje08/Knjiznica.java
+++ src/main/java/vaje08/Knjiznica.java
@@ -6,11 +6,65 @@ public class Knjiznica {
     private int stNaslovov;
     private int stIzvodovNaNaslov;
 
+    private int[][] clanList;
+    private int[] naslovList;
+    private int[] izposojaLog;
+
     public Knjiznica(int stClanov, int stNaslovov, int stIzvodovNaNaslov) {
         this.stClanov = stClanov;
         this.stNaslovov = stNaslovov;
         this.stIzvodovNaNaslov = stIzvodovNaNaslov;
+
+        this.clanList = new int[stClanov][stNaslovov];
+        this.naslovList = new int[stNaslovov];
+        this.izposojaLog = new int[stNaslovov];
+
+        for (int i = 0; i < stNaslovov; i++) {
+            naslovList[i] = stIzvodovNaNaslov;
+        }
     }
 
+    public boolean posodi(int clan, int naslov) {
+        if (naslovList[naslov] <= 0) return false;
+        if (clanList[clan][naslov] >= 1) return false;
 
+        izposojaLog[naslov]++;
+        naslovList[naslov]--;
+        clanList[clan][naslov]++;
+
+        return true;
+    }
+
+    public void clanVrne(int clan) {
+        for (int i = 0; i < stNaslovov; i++) {
+            naslovList[i] += clanList[clan][i];
+            clanList[clan][i] = 0;
+        }
+    }
+
+    public int posojeni(int naslov) {
+        return stIzvodovNaNaslov - naslovList[naslov];
+    }
+
+    public int priClanu(int clan) {
+        int sum = 0;
+
+        for (int i = 0; i < stNaslovov; i++) {
+            sum += clanList[clan][i];
+        }
+
+        return sum;
+    }
+
+    public int najNaslov() {
+        int najNaslov = 0;
+
+        for (int i = 1; i < stNaslovov; i++) {
+            if (izposojaLog[i] > izposojaLog[najNaslov]) {
+                najNaslov = i;
+            }
+        }
+
+        return najNaslov;
+    }
 }
